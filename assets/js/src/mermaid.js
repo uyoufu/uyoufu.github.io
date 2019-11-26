81a57859713db2011675a6a3aa3c0d65df48f551
Galens GAN <gmx_galens@163.com>
2019-11-26 16:06:51 +0800

    edit mermaid

--- assets/js/src/mermaid.js
+++ assets/js/src/mermaid.js
@@ -1,8 +1,4 @@
----
-  layout: null
----
-  {% if site.mermaid.enable %}
-$("<link>").attr({ href: "{{ site.mermaid.stylesheet }}", rel: "stylesheet" }).appendTo("head");
+{% if site.mermaid.enable %}
 document.write('<script src="{{ site.mermaid.src }}"></script>');
 {% if site.mermaid.markdown_expand %}
 $(".language-mermaid").attr("class", "mermaid");
